b560ace896dc30b257745230e670d013ebdaf383
Anthony Yang <ay339@cornell.edu>
2020-05-23 18:19:42 -0400

    Remove dot visitor. Add support for values displayed on edges to indicate branches.

--- src/java/cfg/ir/CFGUtil.java
+++ src/java/cfg/ir/CFGUtil.java
@@ -6,17 +6,21 @@ import java.io.FileReader;
 import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 import cfg.ir.constructor.CFGConstructor;
-import cfg.ir.dot.IrCFGDotUtil;
-import cfg.ir.dot.IrCFGDotUtil.DotData;
+import cfg.ir.nodes.CFGNode;
 import cyr7.cli.OptConfig;
 import cyr7.ir.DefaultIdGenerator;
 import cyr7.ir.IRUtil;
 import cyr7.ir.nodes.IRCompUnit;
 import cyr7.typecheck.IxiFileOpener;
-import polyglot.util.Pair;
+import graph.Edge;
 public final class CFGUtil {
 
     public static void generateDot(
@@ -78,17 +82,32 @@ public final class CFGUtil {
         outputDotForFunctionIR(cfg, writer);
     }
 
-    public static void outputDotForFunctionIR(CFGGraph node, Writer writer) {
-        PrintWriter printer = new PrintWriter(writer);
-        DotData data = IrCFGDotUtil.execute(node);
+    public static void outputDotForFunctionIR(CFGGraph cfg, Writer writer) {
+        final PrintWriter printer = new PrintWriter(writer);
+        final Map<CFGNode, Integer> nodeToLabel = new HashMap<>();
+        final AtomicInteger count = new AtomicInteger();
+        final List<String> nodes = cfg.nodes().stream().map(n -> {
+                final int id = count.getAndIncrement();
+                final String label = "    " + id + " [label =\"" + n.value().toString() + "\"]";
+                nodeToLabel.put(n.value(), id);
+                return label;
+            }).collect(Collectors.toList());
+
+        final List<Edge<CFGNode, Boolean>> edges = new ArrayList<>(cfg.edges());
+
         printer.println("digraph nfa {");
         printer.println("    node [shape=rectangle]");
-        for(String label: data.nodes()) {
-            printer.println("    \""+label+"\"");
+        for(String label: nodes) {
+            printer.println("    "+ label + ";");
         }
         printer.println();
-        for(Pair<String, String> edge: data.edges()) {
-            String edgeName = "    \""+edge.part1()+"\" -> \""+edge.part2()+"\"";
+        for(Edge<CFGNode, Boolean> e: edges) {
+            final int startId = nodeToLabel.get(e.start.value());
+            final int endId = nodeToLabel.get(e.end.value());
+            String edgeName = "    " + startId + " -> " + endId + 
+                            e.value.map(
+                                v -> "[label =\"  " + String.valueOf(v) + "\"]")
+                            .orElse("");
             printer.println(edgeName);
         }
         printer.println("}");
