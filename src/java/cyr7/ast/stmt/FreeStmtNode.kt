a7f7afc8d72f76681a4f163368d373258a0d3f31
Anthony Yang <ay339@cornell.edu>
2020-12-06 06:49:34 -0500

    Update node structure

--- src/java/cyr7/ast/stmt/FreeStmtNode.kt
+++ src/java/cyr7/ast/stmt/FreeStmtNode.kt
@@ -6,12 +6,19 @@ import cyr7.ast.expr.ExprNode
 import cyr7.visitor.AbstractVisitor
 import java_cup.runtime.ComplexSymbolFactory.Location
 
-class FreeStmtNode(location: Location, val expr: ExprNode): AbstractNode(location), StmtNode {
+class FreeStmtNode(location: Location,
+                   val expr: ExprNode,
+                   ): AbstractNode(location), StmtNode {
+    override val children: List<Node>
+        get() = mutableListOf(expr);
+
+    override fun equals(other: Any?): Boolean {
+        return other is FreeStmtNode && other.expr == expr;
+    }
     override fun <T> accept(visitor: AbstractVisitor<T>): T {
         return visitor.visit(this)
     }
-
-    override fun getChildren(): MutableList<Node> {
-        return mutableListOf(expr)
+    override fun hashCode(): Int {
+        return expr.hashCode()
     }
 }
